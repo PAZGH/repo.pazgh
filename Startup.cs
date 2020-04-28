ee604dece3bc62fbf8eb1dc643b98ac7f8886c23
pazgh <apazzif@gmail.com>
2020-04-27 22:31:54 -0400

    update message

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace repo.pazgh
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Azure!");
                 });
             });
         }
